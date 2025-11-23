a917d4f035961ef2b89305a75a79ed51fa9f9bd7
Gus <legus.codes@gmail.com>
2025-11-23 15:31:24 +0100

    remove dataclass from components, they are all pydantic models

--- ecs_architecture/component/position.py
+++ ecs_architecture/component/position.py
@@ -1,25 +1,19 @@
-from dataclasses import dataclass
-
 from ecs_framework.ecs import ComponentProtocol
 from model.hex_coordinate import HexCoordinate, VecF2
 
 
-@dataclass
 class GridPosition(ComponentProtocol):
     cell: HexCoordinate
 
 
-@dataclass
 class GridPositionChanged(ComponentProtocol):
     pass
 
 
-@dataclass
 class WorldPosition(ComponentProtocol):
     point: VecF2
 
 
-@dataclass
 class ScreenPosition(ComponentProtocol):
     point: VecF2
 
--- ecs_architecture/component/render_layer.py
+++ ecs_architecture/component/render_layer.py
@@ -1,8 +1,5 @@
-from dataclasses import dataclass
-
 from ecs_framework.ecs import ComponentProtocol
 
 
-@dataclass
 class RenderLayer(ComponentProtocol):
     z: int
