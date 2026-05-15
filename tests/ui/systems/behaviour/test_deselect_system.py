81327ccfebae97bea6ad54fe61aaef30ca48b2b1
Gus <legus.codes@gmail.com>
2026-05-15 17:42:10 +0200

    add tests to select and deselect system

---
+++ tests/ui/systems/behaviour/test_deselect_system.py
@@ -0,0 +1,75 @@
+from omniecs.world import EntityId, World, WorldFactory
+
+from core.primitives import Color
+from ui.components.behavior import Selectable, Selected, SelectionGroup
+from ui.components.command import UnsetFrameColorCommand
+from ui.events.events import DeselectGroupEvent
+from ui.systems.behaviour_system import DeselectSystem
+
+
+def create_world() -> World:
+    world = WorldFactory.create_world()
+    world.register_system(DeselectSystem())
+    return world
+
+def test_no_event():
+    world = create_world()
+
+    entity_id = world.spawn(Selectable(), Selected(), SelectionGroup(group='group'))
+    world.execute()
+
+    assert world.query_entities(all_of=(Selected,)) == set([entity_id])
+
+def test_no_selectable():
+    world = create_world()
+
+    entity_id = world.spawn(Selected(), SelectionGroup(group='group'))
+    world.push_event(DeselectGroupEvent(group='group', selected=EntityId(1000)))
+    world.execute()
+
+    assert world.query_entities(all_of=(Selected,)) == set([entity_id])
+
+def test_no_selection_group():
+    world = create_world()
+
+    entity_id = world.spawn(Selected(), Selectable())
+    world.push_event(DeselectGroupEvent(group='group', selected=EntityId(1000)))
+    world.execute()
+
+    assert world.query_entities(all_of=(Selected,)) == set([entity_id])
+
+def test_deselect():
+    world = create_world()
+
+    entity_id = world.spawn(Selected(), Selectable(), SelectionGroup(group='group'))
+    world.push_event(DeselectGroupEvent(group='group', selected=EntityId(1000)))
+    world.execute()
+
+    assert world.query_entities(none_of=(Selected,)) == set([entity_id])
+
+def test_deselect_with_command():
+    world = create_world()
+
+    entity_id = world.spawn(Selected(), Selectable(exit=[UnsetFrameColorCommand(Color(0, 0, 0))]), SelectionGroup(group='group'))
+    world.push_event(DeselectGroupEvent(group='group', selected=EntityId(1000)))
+    world.execute()
+
+    assert world.query_entities(all_of=(UnsetFrameColorCommand,), none_of=(Selected,)) == set([entity_id])
+
+def test_deselect_entity_selected():
+    world = create_world()
+
+    entity_id = world.spawn(Selected(), Selectable(), SelectionGroup(group='group'))
+    world.push_event(DeselectGroupEvent(group='group', selected=entity_id))
+    world.execute()
+
+    assert world.query_entities(all_of=(Selected,)) == set([entity_id])
+
+def test_deselect_another_group():
+    world = create_world()
+
+    entity_id = world.spawn(Selected(), Selectable(), SelectionGroup(group='group'))
+    world.push_event(DeselectGroupEvent(group='group2', selected=EntityId(1000)))
+    world.execute()
+
+    assert world.query_entities(all_of=(Selected,)) == set([entity_id])
---
+++ tests/ui/systems/behaviour/test_select_system.py
@@ -0,0 +1,62 @@
+from omniecs.world import World, WorldFactory
+
+from core.primitives import Color
+from ui.components.behavior import Selectable, Selected, SelectionGroup
+from ui.components.command import SetTextColorCommand
+from ui.components.intent import PressIntent
+from ui.events.events import DeselectGroupEvent
+from ui.systems.behaviour_system import SelectSystem
+
+
+def create_world() -> World:
+    world = WorldFactory.create_world()
+    world.register_system(SelectSystem())
+    return world
+
+def test_no_press_intention():
+    world = create_world()
+
+    entity_id = world.spawn(Selectable())
+    world.execute()
+
+    assert world.query_entities(none_of=(Selected,)) == set([entity_id])
+
+def test_no_selectable():
+    world = create_world()
+
+    entity_id = world.spawn(PressIntent())
+    world.execute()
+
+    assert world.query_entities(none_of=(Selected,)) == set([entity_id])
+
+def test_add_selected():
+    world = create_world()
+
+    entity_id = world.spawn(Selectable(), PressIntent())
+    world.execute()
+
+    assert world.query_entities(all_of=(Selected,)) == set([entity_id])
+    assert len(world.get_events()) == 0
+
+def test_add_selected_with_command():
+    world = create_world()
+
+    entity_id = world.spawn(Selectable(enter=[SetTextColorCommand(Color(0, 0, 0))]), PressIntent())
+    world.execute()
+
+    assert world.query_entities(all_of=(Selected, SetTextColorCommand)) == set([entity_id])
+    assert len(world.get_events()) == 0
+
+def test_add_selected_in_group():
+    world = WorldFactory.create_empty_world()
+    world.register_system(SelectSystem())
+
+    entity_id = world.spawn(Selectable(), SelectionGroup(group='group'), PressIntent())
+    world.execute()
+
+    assert world.query_entities(all_of=(Selected,)) == set([entity_id])
+    assert len(world.get_events()) == 1
+    event = world.get_events()[0]
+    assert isinstance(event, DeselectGroupEvent)
+    assert event.group == 'group'
+    assert event.selected == entity_id
